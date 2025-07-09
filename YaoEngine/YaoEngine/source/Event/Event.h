9b3435ff0b31b4ba32d07d50d1c064581ed32199
YaoYao <1302668112@qq.com>
2025-07-09 16:42:23 +0800

    update Event.h

--- YaoEngine/YaoEngine/source/Event/Event.h
+++ YaoEngine/YaoEngine/source/Event/Event.h
@@ -1,1 +1,29 @@
-"初始化文件"
+#pragma once
+enum class EventType {
+	None=0,
+	AppClose,		
+	WindowResize,	WindowClose,	
+	MousePress,		MouseRelease,	Mousescoll,
+	KeyPress,		KeyRelease,		
+	
+};
+class Event
+{
+public:
+	virtual ~Event() = default;
+
+	virtual char* Getname()=0;
+private:
+	char* Name;
+	EventType type;
+};
+class WindowResize :public Event {
+public:
+	//~WindowResize() {};
+	WindowResize(int x, int y) { wide= x; height = y; }
+	virtual char* Getname()override {  return "s"; }
+	int wide, height;
+private:
+	
+};
+
