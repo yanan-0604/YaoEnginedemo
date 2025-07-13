737c407c87e764a6b315b87e838f4225ca0bf0a3
YaoYao <1302668112@qq.com>
2025-07-14 01:39:57 +0800

    Add Scene

---
+++ YaoEngine/YaoEngine/source/Scene/Scene.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<entt.hpp>
+#include"../UUID/UUID.h"
+#include<string>
+class Scene {
+public:
+	Scene();
+	~Scene();
+
+	
+private:
+	
+};
+class Component {
+public:
+	
+
+
+private:
+};
+class Entity {
+public:
+	
+
+private:
+	UUID* uuid;
+};
